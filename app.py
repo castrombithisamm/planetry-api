c429920037709923b438d5e406613007351ac917
castrombithisamm <castro.mbithii@gmail.com>
2024-03-13 16:24:49 +0300

    changes made to rest api

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return  'Hello, World!'
+    return 'Hello, World!'
+
+
+@app.route('/super_simple')
+def super_simple():
+    return jsonify(message='Hello from the Planetary API. booyah'), 200
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='The requested item was not found'), 404
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message='Sorry ' + name + ', you are not old enough'), 401
+    else:
+        return jsonify(message='Welcome ' + name + ', you are old enough')
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message='Sorry ' + name + ', you are not old enough'), 401
+    else:
+        return jsonify(message='Welcome ' + name + ', you are old enough')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
